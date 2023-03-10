ec4eace46b5b1ba6ba5cec684c11f61f62994a5d
leebean <74585673+delight-HK3@users.noreply.github.com>
2023-03-10 21:22:47 +0900

    Update callstack.js
    순서 기록

--- 2week/ch1/callstack.js
+++ 2week/ch1/callstack.js
@@ -1,12 +1,12 @@
-function first() {
-  second();
-  console.log('첫 번째');
+function first() { 
+  second(); // 2 번 
+  console.log('첫 번째'); // 6 번
 }
 function second() {
-  third();
-  console.log('두 번째');
+  third(); // 3 번
+  console.log('두 번째'); // 5 번
 }
-function third() {
-  console.log('세 번째');
+function third() { 
+  console.log('세 번째'); // 4 번
 }
-first();
+first(); // 1 번
