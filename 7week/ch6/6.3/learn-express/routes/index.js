700563cf783b15493a11b2e7a8823a4b8321c4e7
leebean <dabin310153@naver.com>
2023-04-14 21:48:19 +0900

    change

--- 7week/ch6/6.3/learn-express/routes/index.js
+++ 7week/ch6/6.3/learn-express/routes/index.js
@@ -7,4 +7,7 @@ router.get('/', (req, res) => {
   res.send('Hello, Express');
 });
 
+router.get('/user/:id', function(req, res) {
+  console.log(req.params, req.query);
+});
 module.exports = router;
